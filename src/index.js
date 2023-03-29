4612d1289654295647cc02e27ffeb77c6eb5b0c8
Natalia Grela <nataliagrela83@mail.com>
2023-03-29 21:23:47 +0200

    hw 11

--- src/index.js
+++ src/index.js
@@ -1,1 +1,72 @@
-const test = document.querySelector('.test')
+import './css/styles.css';
+import { fetchPicture } from './js/fetchPicture';
+import axios from 'axios';
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
+const inputSearch = document.querySelector('[name="searchQuery"]')
+const btn = document.querySelector('.btn')
+let page = 1;
+let query = ''
+
+loadMoreBtn.style.display = 'none';
+
+const handleChange = e => { 
+query = e.target.value
+}
+
+const handleClick = async e => {
+    e.preventDefault()
+    const data = await fetchPicture(40, query)
+    console.log(data);
+    createGallery(data.hits)
+}
+
+const createGallery = (data) => {
+    console.log(data);
+    const images = data.map(image => {
+        const { webformatURL, largeImageURL, tags, likes, views, comments, downloads} = image
+        
+        return(`<div class="photo-card">
+  <img src="${webformatURL}" alt="" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+    </p>
+  </div>
+</div>`)
+    }).join(' ')
+    gallery.innerHTML = images
+    console.log(images);
+
+ }
+ 
+ 
+btn.addEventListener ('click', handleClick)
+inputSearch.addEventListener ('input', handleChange)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
